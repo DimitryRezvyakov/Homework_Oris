0a62c4053dd759b72d641a187336b54882476c3d
Dimitry Rezvyackov <dimrez199@gmail.com>
2025-10-11 11:29:17 +0300

    Homework
    MiniHttpServer

---
+++ Homework_3/MiniHttpServer/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Net;
+using System.Runtime.InteropServices.JavaScript;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using MiniHttpServer.Servers;
+using MiniHttpServer.Settings;
+using MiniHttpServer.Shared;
+
+namespace MiniHttpServer;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var settingsValues = SettingsProvider.Settings;
+
+        var server = new HttpServer();
+
+        server.Command = async (context) =>
+        {
+            var response = context.Response;
+
+            string? path = context.Request.Url?.AbsolutePath;
+
+            byte[]? responseBytes;
+
+            if (path == null || path == "/")
+                responseBytes = GetResponseBytes.Invoke($"Public/index.html");
+
+            else
+            {
+                responseBytes = GetResponseBytes.Invoke($"{path}/");
+            }
+
+            if (responseBytes == null)
+            {
+                response.StatusCode = 404;
+                response.OutputStream.Close();
+            }
+
+            else
+            {
+                response.ContentType = GetContentType.Invoke(path);
+                response.ContentLength64 = responseBytes.Length;
+
+                using Stream output = response.OutputStream;
+
+                await output.WriteAsync(responseBytes);
+                await output.FlushAsync();
+            }
+        };
+
+        Task.Run(() =>
+        {
+            server.Start();
+        });
+
+        while (true)
+        {
+            if (Console.ReadLine() == "/stop")
+            {
+                server.cts.Cancel();
+                server.Stop();
+                break;
+            }
+        }
+    }
+}
---
+++ Homework_3/MiniHttpServer/Servers/HttpServer.cs
@@ -0,0 +1,82 @@
+﻿using MiniHttpServer.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniHttpServer.Servers
+{
+    public class HttpServer
+    {
+
+        private HttpListener _listener;
+        public SettingsModel _settings { get; init; }
+
+
+        public CancellationTokenSource cts = new CancellationTokenSource();
+        public bool isRunning => _listener.IsListening;
+        public Action<HttpListenerContext>? Command { get; set; }
+
+        public HttpServer()
+        {
+            _listener = new HttpListener();
+            _settings = SettingsProvider.Settings;
+        }
+
+        public void Start()
+        {
+
+
+            try
+            {
+                _listener.Prefixes.Add(_settings.Prefix);
+                _listener.Start();
+                Receive();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void Stop()
+        {
+            if (!_listener.IsListening)
+            {
+                Console.WriteLine("Сервер еще не запущен");
+            }
+
+            _listener.Stop();
+        }
+
+        private void Receive()
+        {
+            var result = _listener.BeginGetContext(new AsyncCallback(ListenerCallback), _listener);
+        }
+
+        private void ListenerCallback(IAsyncResult ar)
+        {
+            if (_listener.IsListening)
+            {
+                if (cts.IsCancellationRequested) return;
+
+                var context = _listener.EndGetContext(ar);
+
+                if (Command != null)
+                {
+                    Command(context);
+                }
+
+                else
+                {
+                    var request = context.Request;
+
+                    Console.WriteLine($"{request.Url}");
+                }
+
+                Receive();
+            }
+        }
+    }
+}
